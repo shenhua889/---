4eee4aad8038e18c16d03d2a93f9e56950ca88d6
shenhua889 <shenhua889@126.com>
2017-11-23 15:17:32 +0800

    添加项目文件。

---
+++ 创意研发室-待出库统计/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 创意研发室_待出库统计
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
